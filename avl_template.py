b29be8d745472688c628f1c13c91e692e8e0d72d
Omer Shalev <omershalev1@mail.tau.ac.il>
2024-02-19 10:08:51 +0200

    fixed delete and join edge cases

--- avl_template.py
+++ avl_template.py
@@ -4,7 +4,6 @@
 # id2      - 207765181
 # name2    - omer shalev
 
-
 """A class represnting a node in an AVL tree"""
 
 
@@ -17,6 +16,7 @@ class AVLNode(object):
     """
 
     def __init__(self, key=None, value=None):
+        #AVLNode builder. initialize a node in O(1).
         self.key = key
         self.value = value
         self.left = None
@@ -80,6 +80,7 @@ class AVLNode(object):
     """
 
     def get_left(self):
+        #time complexity O(1)
         return self.left
 
     """returns the right child
@@ -89,6 +90,7 @@ class AVLNode(object):
     """
 
     def get_right(self):
+        #time complexity O(1)
         return self.right
 
     """returns the parent 
@@ -98,6 +100,7 @@ class AVLNode(object):
     """
 
     def get_parent(self):
+        #time complexity O(1)
         return self.parent
 
     """returns the key
@@ -107,6 +110,7 @@ class AVLNode(object):
     """
 
     def get_key(self):
+        #time complexity O(1)
         return self.key
 
     """returns the value
@@ -116,6 +120,7 @@ class AVLNode(object):
     """
 
     def get_value(self):
+        #time complexity O(1)
         return self.value
 
     """returns the height
@@ -125,6 +130,7 @@ class AVLNode(object):
     """
 
     def get_height(self):
+        #time complexity O(1)
         return self.height
 
     """sets left child
@@ -134,6 +140,7 @@ class AVLNode(object):
     """
 
     def set_left(self, node):
+        #time complexity O(1)
         self.left = node
         return None
 
@@ -144,6 +151,7 @@ class AVLNode(object):
     """
 
     def set_right(self, node):
+        #time complexity O(1)
         self.right = node
         return None
 
@@ -154,6 +162,7 @@ class AVLNode(object):
     """
 
     def set_parent(self, node):
+        #time complexity O(1)
         self.parent = node
         return None
 
@@ -164,6 +173,7 @@ class AVLNode(object):
     """
 
     def set_key(self, key):
+        #time complexity O(1)
         self.key = key
         return None
 
@@ -174,6 +184,7 @@ class AVLNode(object):
     """
 
     def set_value(self, value):
+        #time complexity O(1)
         self.value = value
         return None
 
@@ -184,6 +195,7 @@ class AVLNode(object):
     """
 
     def set_height(self, h):
+        #time complexity O(1)
         self.height = h
         return None
 
@@ -194,6 +206,7 @@ class AVLNode(object):
     """
 
     def is_real_node(self):
+        #time complexity O(1)
         return self.key != None
 
 
@@ -209,6 +222,7 @@ class AVLTree(object):
     """
 
     def __init__(self):
+        #AVLTree builder. initialize an empty tree with a virtual Node as root in O(1).
         self.root = AVLNode()
         self.tree_size = 0
 
@@ -223,12 +237,13 @@ class AVLTree(object):
     """
 
     def search(self, key):
-        # Searched for key in tree. Time complexity O(logn)
+        # Search for key in tree. calls the function searchNode which does binary search. Time complexity O(logn).
         node = self.searchNode(self.root, key)
         return node if node.is_real_node() else None
 
     def searchNode(self, node, key):
         # Search a key in subtree. if key exists return node, if not, return the the virtual node that in the key's position.
+        #Using binary search. time complexity O(logn)
         while node.is_real_node():
             if node.key == key:
                 return node
@@ -250,10 +265,12 @@ class AVLTree(object):
     """
 
     def insert(self, key, val):
+        #time complexity O(logn) for insertBST and at most 1 gilgul O(1) hence in total O(logn)
         self.tree_size += 1
         node = self.insertBST(key, val)
         parent = node.get_parent()
         cnt_gilgul = 0
+        #checking if the new tree after the insertion is a valid AVL tree.
         while parent is not None:
             parent_height = self.calcHeight(parent)
             parent_bf = self.bf(parent)
@@ -264,12 +281,15 @@ class AVLTree(object):
                 cnt_gilgul += 1
                 parent = parent.get_parent()
             else:
+                #As mentioned in class, after one gilgul at most, the tree will be a valid AVL tree.
                 cnt_gilgul += self.gilgul(parent, parent_bf)
                 return cnt_gilgul
         return cnt_gilgul
 
     def insertBST(self, key, val):
+        #Search the location where the new node should be inserted.returns a Virtual node in this position. O(logn).
         node = self.searchNode(self.root, key)
+        #updating the virtual node's values and adding two virtual sons.
         node.set_key(key)
         node.set_value(val)
         node.set_right(AVLNode())
@@ -280,10 +300,11 @@ class AVLTree(object):
         return node
 
     def bf(self, node):
-        # Calculates the balance factor of a node.
+        # Calculates the balance factor of a node. O(1)
         return node.get_left().get_height() - node.get_right().get_height()
 
     def calcHeight(self, node):
+        #Calculate the height of a node based on the heights of its sons. O(1)
         return (max(node.get_left().get_height(), node.get_right().get_height()) + 1)
 
     """deletes node from the dictionary
@@ -295,28 +316,32 @@ class AVLTree(object):
     """
 
     def gilgul(self, node, node_bf):
+        #making rotations to fix problematic nodes with big BF. maximum rotations number is 2, time complexity O(n)
         cnt = 0
         if node_bf == 2:
             left_son = node.get_left()
             left_son_bf = self.bf(left_son)
+            #Checking if two rotations are needed.
             if left_son_bf < 0:
                 cnt += self.gilgulLeft(left_son)
             cnt += self.gilgulRight(node)
         if node_bf == -2:
             right_son = node.get_right()
             right_son_bf = self.bf(right_son)
+            #checking if two rotations are needed.
             if right_son_bf > 0:
                 cnt += self.gilgulRight(right_son)
             cnt += self.gilgulLeft(node)
         return cnt
 
     def gilgulRight(self, node):
+		#changing pointers, the number of changes is fixed. time complexity O(1)
         left_son = node.get_left()
         node.set_left(left_son.get_right())
         node.get_left().set_parent(node)
         left_son.set_right(node)
         left_son.set_parent(node.get_parent())
-        # check if left son id on right ot left son of node's parent
+        # check if left son is a right or left son of node's parent
         if left_son.get_parent() is not None and left_son.get_key() < left_son.get_parent().get_key():
             left_son.get_parent().set_left(left_son)
         elif left_son.get_parent() is not None:
@@ -329,12 +354,13 @@ class AVLTree(object):
         return 1
 
     def gilgulLeft(self, node):
+        #changing pointers, the number of changes is fixed. time complexity O(1)
         right_son = node.get_right()
         node.set_right(right_son.get_left())
         node.get_right().set_parent(node)
         right_son.set_left(node)
         right_son.set_parent(node.get_parent())
-        # check if right son id on right ot left son of node's parent
+        # check if right son is a right or left son of node's parent
         if right_son.get_parent() is not None and right_son.get_key() < right_son.get_parent().get_key():
             right_son.get_parent().set_left(right_son)
         elif right_son.get_parent() is not None:
@@ -347,6 +373,10 @@ class AVLTree(object):
         return 1
 
     def delete(self, node):
+        #deleting a node from the tree. using BSTdelete to delete the correct node, time complexity O(logn)
+        #go up the tree from the deleted node and calling gilgul to fix problems that may have happened.O(logn)
+        #return the number of adjusments made to correct the tree.
+        #Time complexity O(logn)
         self.tree_size -= 1
         cnt_gilgul = 0
         parent = self.BSTdelete(node)
@@ -366,11 +396,16 @@ class AVLTree(object):
         return cnt_gilgul
 
     def minSubTree(self, node):
+        #find the minimum key in the sub tree where node is the root. Time complexity O(h) = O(logn)
         while node.get_left().is_real_node():
             node = node.get_left()
         return node
 
     def succesor(self, node):
+        #find the successor of a given node in a tree.
+        #Going up or down the tree to find the successor key.
+        #calling the function minSubTree which is O(logn)
+        #Time complexity O(h) = O(logn)
         if node.get_right().is_real_node():
             return self.minSubTree(node.get_right())
         parent = node.get_parent()
@@ -380,13 +415,21 @@ class AVLTree(object):
         return parent
 
     def BSTdelete(self, node):
+        #deleting a node from the tree.
+        #changes a fixed amount of pointers, calling the function succesor which is O(logn)
+        #time complexity O(logn)
+        
+        #the node has two real sons.
         if node.get_left().is_real_node() and node.get_right().is_real_node():
             suc = self.succesor(node)
+            #deleting the successor of node from the tree. suc cannot have two children.
             parent = self.deleteEasy(suc)
+            #placing suc in the location of node in the tree.
             suc.set_right(node.get_right())
             suc.set_left(node.get_left())
             node.get_left().set_parent(suc)
             node.get_right().set_parent(suc)
+            #if deleting the root.
             if node.get_parent() is None:
                 self.root = suc
             elif node.get_parent().get_key() < suc.get_key():
@@ -395,40 +438,56 @@ class AVLTree(object):
                 node.get_parent().set_left(suc)
             suc.set_parent(node.get_parent())
             suc.set_height(node.get_height())
+        #the node has less than 2 real children
         else:
             parent = self.deleteEasy(node)
-        return parent if parent != node else suc
+        if parent is node:
+            suc.set_height(node.get_height())
+            parent = suc
+        return parent
 
     def deleteEasy(self, node):
+        #handeling deleting a node when it has only one or zero childs.
+        #returns the parent of the deleted node.
+        #only changes a fixed number of pointers and therefore the time complexity is O(1)
         left = node.get_left().is_real_node()
         parent = node.get_parent()
+        #only has right child or no childs
         if not left:
+            #the node is the root of a tree 
             if parent is None:
                 self.root = node.get_right()
                 node.get_right().set_parent(None)
                 node.set_right(None)
+            #the node is not the root
             else:
+                #the node is left son of his parent
                 if node.get_key() < parent.get_key():
                     parent.set_left(node.get_right())
                     node.get_right().set_parent(parent)
                     node.set_right(None)
                     node.set_parent(None)
+                #the node is right son of his parent
                 else:
                     parent.set_right(node.get_right())
                     node.get_right().set_parent(parent)
                     node.set_right(None)
                     node.set_parent(None)
+        #has only left child. this function won't be called when node has 2 children.
         elif left:
+            #node is the root of a tree
             if parent is None:
                 self.root = node.get_left()
                 node.get_left().set_parent(None)
                 node.set_left(None)
             else:
+                #node is the left son of his parent
                 if node.get_key() < parent.get_key():
                     parent.set_left(node.get_left())
                     node.get_left().set_parent(parent)
                     node.set_left(None)
                     node.set_parent(None)
+                #node is the right son of his parent
                 else:
                     parent.set_right(node.get_left())
                     node.get_left().set_parent(parent)
@@ -443,6 +502,10 @@ class AVLTree(object):
     """
 
     def avl_to_array(self):
+        #initalizing an empty array with the size of the tree.O(n)
+        #calling minSubTree to find the minimum key in the tree O(logn)
+        #calling successor for each node until going through all the nodes. As seen in class O(n+h) = O(n+logn)
+        #time complexity O(n)
         arr = [0 for i in range(self.tree_size)]
         node = self.minSubTree(self.root)
         i = 0
@@ -461,6 +524,7 @@ class AVLTree(object):
     """
 
     def size(self):
+		#time complexity O(1)
         return self.tree_size
 
     """splits the dictionary at the i'th index
@@ -475,6 +539,9 @@ class AVLTree(object):
     """
 
     def split(self, node):
+        #going up the tree from node. O(h) = O(logn)
+        #in every iteration, calling join.
+        #as we saw in class, time complexity is O(logn)
         key = node.get_key()
         left = AVLTree()
         left.root = node.get_left()
@@ -518,18 +585,29 @@ class AVLTree(object):
     """
 
     def join(self, tree2, key, val):
+        # creates a node with key and val and use it to call joinWithNode.
+        #checks for the subtree with the bigger keys to stand in the requirments of joinWithNode.
+        #time complexity O(abs(self.get_height() - tree2.get_height()) + 1))
         node = AVLNode(key,val)
-        if tree2.root.get_key() > key:
+        if not tree2.root.is_real_node() or tree2.root.get_key() > key:
             return self.joinWithNode(node, tree2)
         diff = tree2.joinWithNode(node, self)
         self.root = tree2.get_root()
         self.tree_size = tree2.size()
+        tree2.root = None
         return diff
 
     def joinWithNode(self, node, tree2):
-        smaller = self.get_root()
-        bigger = tree2.get_root()
+        #the function requires that tree2 will have smaller values than in self.
+        #going down the heigher tree O(diff)
+        #Joining the two subtrees with the same heights by changing pointers. O(1)
+        #going up the joined tree and fix nodes if needed. O(diff)
+        #time complexity O(diff) = O(abs(smaller.get_height() - bigger.get_height()) + 1)
+        
+        smaller = self.root
+        bigger = tree2.root
         diff = abs(smaller.get_height() - bigger.get_height()) + 1
+        #smaller is lower than bigger
         if smaller.get_height() < bigger.get_height():
             h = smaller.get_height()
             curr = bigger
@@ -545,6 +623,7 @@ class AVLTree(object):
                 self.root = bigger
             else:
                 self.root = node
+        # smaller and bigger have the same height
         elif smaller.get_height() == bigger.get_height():
             h = smaller.get_height()
             node.set_left(smaller)
@@ -552,6 +631,7 @@ class AVLTree(object):
             smaller.set_parent(node)
             bigger.set_parent(node)
             self.root = node
+        # bigger is lower than smaller
         else:
             h = bigger.get_height()
             curr = smaller
@@ -571,6 +651,8 @@ class AVLTree(object):
         node.set_height(h+1)
         parent = node.get_parent()
         self.tree_size += tree2.tree_size + 1
+        #checking for every node in the path from node to the root of the new joined tree if it is valid.
+        #The number of nodes checked is at most diff.
         while parent is not None:
             parent_height = self.calcHeight(parent)
             parent_bf = self.bf(parent)
@@ -584,7 +666,6 @@ class AVLTree(object):
                 return diff
         return diff
 
-    # return root of joined tree
 
     """returns the root of the tree representing the dictionary
 
@@ -593,14 +674,15 @@ class AVLTree(object):
     """
 
     def get_root(self):
-        return self.root
-
+        #time complexity O(1)
+        return self.root if self.root.is_real_node() else None
+    
 
 def test():
     tree = AVLTree()
-    tree.insert(8, "a")
-    tree.insert(4, "b")
-    tree.insert(5, "c")
+    print(tree.insert(8, "a"))
+    print(tree.insert(4, "b"))
+    print(tree.insert(5, "c"))
     tree.insert(10, "d")
     tree.insert(9, "e")
     tree.insert(12, "4")
@@ -608,18 +690,22 @@ def test():
 
     tree.root.display()
     tree2 = AVLTree()
-    tree2.insert(15, "a")
-    tree2.insert(17, "b")
-    #tree2.insert(20, "b")
-    #tree2.insert(22, "b")
-    #tree2.insert(25, "b")
+    print(tree.root.get_height())
+    # print(tree2.root.height)
+    # tree2.insert(15, "a")
+    # tree2.insert(17, "b")
+    # tree2.insert(20, "b")
+    # tree2.insert(22, "b")
+    # tree2.insert(25, "b")
     tree2.root.display()
-    tree.join(tree2, 13, "")
-    tree.root.display()
-    t1, t2= tree.split(tree.search(10))
-    t1.root.display()
-    t2.root.display()
-    print(t2.root.parent)
-
+    print(tree2.join(tree, 13, ""))
+    tree2.root.display()
+    tree2.delete(tree2.search(11))
+    tree2.root.display()
+    print(tree2.search(9).get_height())
+    # t1, t2= tree.split(tree.search(9))
+    # t1.root.display()
+    # t2.root.display()
+    # print(tree.root.parent)
 
 test()
